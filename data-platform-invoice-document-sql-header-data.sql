3195247633cb610e900c0016fc0f1811bb6a6b7b
Yurino Oga <yurino.o@latona.io>
2022-11-23 15:41:53 +0900

    release

--- data-platform-invoice-document-sql-header-data.sql
+++ data-platform-invoice-document-sql-header-data.sql
@@ -14,7 +14,8 @@ CREATE TABLE `data_platform_invoice_document_header_data`
     `AccountingPostingDate`      date DEFAULT NULL,
     `InvoiceDocumentIsCancelled` tinyint(1) DEFAULT NULL,
     `CancelledInvoiceDocument`   int(16) DEFAULT NULL,
-    `IsExportDelivery`           tinyint(1) DEFAULT NULL,
+    `IsExportImportDelivery`     tinyint(1) DEFAULT NULL,
+    `HeaderBillingIsConfirmed`   tinyint(1) DEFAULT NULL,
     `HeaderBillingConfStatus`    varchar(2) DEFAULT NULL,
     `TotalNetAmount`             float(13) DEFAULT NULL,
     `TotalTaxAmount`             float(13) DEFAULT NULL,
@@ -29,14 +30,16 @@ CREATE TABLE `data_platform_invoice_document_header_data`
     `ExternalReferenceDocument`  varchar(35) DEFAULT NULL,
     `DocumentHeaderText`         varchar(200) DEFAULT NULL,
     
-    PRIMARY KEY (`InvoiceDocument`)
+    PRIMARY KEY (`InvoiceDocument`),
     
     CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataBillToParty_fk` FOREIGN KEY (`BillToParty`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
     CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataBillFromParty_fk` FOREIGN KEY (`BillFromParty`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
+    CONSTRAINT `DataPlatformInvoiceDocumentHeaderBillToCountry_fk` FOREIGN KEY (`BillToCountry`) REFERENCES `data_platform_country_country_data` (`Country`),
+    CONSTRAINT `DataPlatformInvoiceDocumentHeaderBillFromCountry_fk` FOREIGN KEY (`BillFromCountry`) REFERENCES `data_platform_country_country_data` (`Country`),
     CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataTransactionCurrency_fk` FOREIGN KEY (`TransactionCurrency`) REFERENCES `data_platform_currency_currency_data` (`Currency`),    
     CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataIncoterms_fk` FOREIGN KEY (`Incoterms`) REFERENCES `data_platform_incoterms_incoterms_data` (`Incoterms`),
     CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataPaymentTerms_fk` FOREIGN KEY (`PaymentTerms`) REFERENCES `data_platform_payment_terms_payment_terms_data` (`PaymentTerms`),
-    CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataPaymentMethod_fk` FOREIGN KEY (`PaymentMethod`) REFERENCES `data_platform_payment_method_payment_method_data` (`PaymentMethod`),
+    CONSTRAINT `DataPlatformInvoiceDocumentHeaderPartnerDataPaymentMethod_fk` FOREIGN KEY (`PaymentMethod`) REFERENCES `data_platform_payment_method_payment_method_data` (`PaymentMethod`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
